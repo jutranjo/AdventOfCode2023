6c681b2d6929c5b54a00cffb4302779d65d0caf4
Ivo Pačnik <pacnik@gmail.com>
2023-12-08 01:38:39 +0100

    Wrote cases, wrote tests for cases

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::fs;
-use _05::solve_part1;
+use _05::{solve_part1, solve_part2};
 
 fn main() {
-    let file_location = "input.txt";
+    let file_location = "example.txt";
     let example = fs::read_to_string(file_location).expect(&format!("Couldn't open file: {}",file_location));
 
     solve_part1(&example);
+    solve_part2(&example);
 }
